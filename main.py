a4f0e1092056840ebdf5fd85f558b5761d44f6b0
hyunsungkim <hands0924@gmail.com>
2025-06-12 18:54:55 +0900

    final

--- main.py
+++ main.py
@@ -113,7 +113,20 @@ def send_sms(phone, name, inquiry):
             SOLAPI_API_SECRET
         )
         
-        text = f"{inquiry} {name}님, OX퀴즈에 참여해주셔서 감사합니다. 정답은 O입니다."
+        text = f"""[포용적 금융서비스, 프리즘지점]
+{name}님, 만사형통 프리즘 부적 이벤트에 참여해주셔서 감사합니다! 
+
+프리즘지점은 퀴어 당사자와 앨라이 보험설계사가 함께하는 보험 조직입니다. 모두를 위한 미래보장을 꿈꾸며, 금융의 경계를 넘어 연대합니다.
+
+선택해주신 프리즘지점에서, {inquiry} 문의에 반가운 마음을 전하며, 유용한 소식과 답변 안내드릴 수 있도록 곧 다시 연락드리겠습니다. 고맙습니다!
+
+프리즘지점 드림
+[보험상담 및 채용문의]
+https://litt.ly/prism.fin
+
+앞으로 소식은
+[인스타그램] 팔로우해주세요!
+www.instagram.com/prism.fin"""
         logger.info(f"생성된 메시지 내용: {text}")
         
         # RequestMessage 모델 사용
